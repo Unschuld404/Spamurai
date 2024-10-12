2b1e37e98871cc7b2cc069582c93992a6822c22a
Unschuld404 <git@unschuld.org>
2024-10-12 20:57:21 +0200

    Header zur Navigation eingerichtet

--- spamurai/src/App.vue
+++ spamurai/src/App.vue
@@ -4,8 +4,6 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/spamurai-icon.png" width="125" height="125" />
-
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Login</RouterLink>
@@ -19,52 +17,21 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-color: var(--accent-color);
+  width: 100vw;
+  height: 5vh;
+  line-height: 5vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper {
+  width: 50vh;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  display: flex;
+  justify-content: space-around;
 }
 </style>
