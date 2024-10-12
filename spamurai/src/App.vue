a1d405d18468f0cdf26c06d655c1ee49c612f671
Unschuld404 <git@unschuld.org>
2024-10-12 19:54:55 +0200

    Nicht mehr benötigte Projektseiten entfernt

--- spamurai/src/App.vue
+++ spamurai/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,12 +7,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/spamurai-icon.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/">Login</RouterLink>
         <RouterLink to="/search">Suche</RouterLink>
         <RouterLink to="/edit">Bearbeiten</RouterLink>
       </nav>
