778510a3c3154df61e5b3d8e37a65daa1a2508ac
Lisa Wichmann <git@unschuld.org>
2025-08-17 13:38:14 +0200

    Errortranslator

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 
-import { ref } from 'vue'
+import { ref } from 'vue';
+import spamuraiLogo from '@/assets/spamurai_logo.png';
 
 const url = 'https://api.curatest.de/auth/v1/user';
-
 const hasError = ref(false);
+// das ref<string> muss ich noch recherchieren
+const errorMessage = ref<string>('')
 
 async function login() {
-
   const username = (document.getElementById('username') as HTMLInputElement).value;
   const password = (document.getElementById('password') as HTMLInputElement).value;
 // Username:Passwort → Base64
@@ -25,21 +26,34 @@ async function login() {
 
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
     const data = await res.text();
-    console.log('OK:', data);
     localStorage.setItem('auth', '1')
     localStorage.setItem('token', data)
     location.href = '/admin'
   } catch (err) {
     hasError.value = true
     setTimeout(() => (hasError.value = false), 500)
-    console.error('Login-Fehler:', err);
+    console.error('Login-Fehler:', err)
+    errorMessage.value = translateError(err)
   }
+}
+
+function translateError(err: unknown): string {
+  if (err instanceof Error) {
+    if (err.message.startsWith('HTTP 401')) return 'Benutzername oder Passwort falsch.'
+    if (err.message.startsWith('HTTP 500')) return 'Serverfehler, bitte später versuchen.'
+    return 'Unbekannter Fehler: ' + err.message
+  }
+  return 'Es ist ein unbekannter Fehler aufgetreten.'
 }
 
 </script>
 
 <template>
   <main>
+    <img :src="spamuraiLogo" alt="Spamurai"/>
+    <div class="box">
+      <p v-if="errorMessage">{{ errorMessage }}</p>
+    </div>
     <form @submit.prevent="login">
       <input type="text" placeholder="Benutzername" id="username"/>
       <input type="password" placeholder="Passwort" id="password"/>
@@ -50,15 +64,33 @@ async function login() {
 
 <style scoped>
 
+.box {
+  height: 3rem;
+  padding: 0;
+  margin-top: 2vh;
+}
+ p {
+   margin: 0;
+ }
+
 button {
-  margin-top: 2rem;
+  margin-top: 1rem;
   transition: background-color 0.3s;
 }
 
 button.error {
   background-color: var(--color-accent);
-  transition: background-color 0.1s;
+  transition: background-color 0.2s;
   color: var(--color-secondary);
+}
+
+img {
+  max-width: 50vw;
+  max-height: 30vh;
+}
+
+p {
+  color: var(--color-accent);
 }
 
 </style>
