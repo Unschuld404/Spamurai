7696ee00a60b95cd30e146ed69e1e55f697e1ff3
Lisa Wichmann <git@unschuld.org>
2025-08-17 19:24:57 +0200

    listing with fetch data

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -4,43 +4,93 @@ import { ref, watch } from 'vue'
 
 const needle = ref('')
 const timer = ref<number | null>(null)
-
-function whatIsNeedle() {
-  console.log(needle.value);
-}
+const url = 'https://api.blackserver.de/spamurai/search/'
+const results = ref<string[]>([])
 
 watch(needle, () => {
   if (timer.value) clearTimeout(timer.value)
-  timer.value = setTimeout(whatIsNeedle, 500) as unknown as number
+  timer.value = setTimeout(search, 500) as unknown as number
 })
+
+async function search() {
+  const searchUrl = url + needle.value
+
+  try {
+    const res = await fetch(searchUrl, {
+    method: 'GET',
+    headers: {
+      ContentType: "application/json; charset=UTF-8",
+      Authorization: `Bearer `+localStorage.getItem('token'),
+    },
+  });
+
+  if (!res.ok) throw new Error(`HTTP ${res.status}`);
+
+  results.value = await res.json()
+  console.log("Suche erfolgreich.", results.value)
+  }
+  catch (err) {
+    console.error("Search-Fehler:", err);
+  }
+}
 
 </script>
 
 <template>
   <div class="container">
-    <input type="search" placeholder="Suche" id="needle" v-model="needle"/>
-    <div class="listing"></div>
+    <input type="search" placeholder="Suche" id="needle" v-model="needle" />
+    <div class="listing">
+      <ul>
+        <li v-for="(item, i) in results" :key="i">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped>
 
-.container {
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  border-bottom: 1px solid var(--color-primary);
+  height: 4rem;
+  font-size: 1.5rem;
   display: flex;
-  flex-direction: column-reverse;
-  border: 1px solid green;
-  height: 100%;
+  align-items: center;
+  padding-left: 1rem;
+}
+
+li:last-of-type {
+  border-bottom: none;
 }
 
 .listing {
-  border: 1px solid saddlebrown;
+  overflow-y: scroll;
+}
+
+.container {
+  display: flex;
+  flex-direction: column-reverse;
   height: 100%;
 }
 
 input {
-  border: 1px solid bisque;
-  margin-top: 2rem;
+  color: var(--color-secondary);
+  background-color: var(--color-primary);
+  border: 1px solid var(--color-secondary);
+  border-radius: 2rem;
+  margin: 2rem 1rem 0 1rem;
   text-align: center;
+}
+
+input::placeholder {
+  color: var(--color-background);
 }
 
 </style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -9,11 +9,10 @@ function logout() {
 }
 
 function tokenInfo() {
-  console.log(localStorage.getItem('token'))
-  alert("Dein Token lautet: "+ localStorage.getItem('token'));
+  console.log("Dein Token lautet: "+ localStorage.getItem('token'));
 }
 
-const url = 'https://api.curatest.de/webapi/auth/check'
+const url = 'https://api.blackserver.de/service/analyze/auth'
 
 async function check() {
   console.log("check")
@@ -31,11 +30,11 @@ async function check() {
     const data = await res.json();
     localStorage.setItem('check', data)
     console.log(data);
-    alert("Du bist noch angemeldet.");
+    console.log("Du bist noch angemeldet.");
   }
   catch (err) {
     console.error('Check-Fehler:', err);
-    alert("Du bist nicht mehr angemeldet.");
+    console.log("Du bist nicht mehr angemeldet.");
   }
 }
 
