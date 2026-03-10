200a5d50041354bb9f9848f745a10e314c904867
Lisa Wichmann <git@unschuld.org>
2026-03-10 19:20:37 +0100

    create new email working

--- src/views/NewEmail.vue
+++ src/views/NewEmail.vue
@@ -1,60 +1,72 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { getDomains } from '@/api/domain.api.ts'
-import { createEmail } from '@/api/createEmail.api.ts'
-import { getPreferences } from '@/api/preferences.api.ts'
-import { getConfig } from '@/api/config.api.ts'
-import type { Preferences } from '@/types/preferences.type.ts'
-import type { Host, Domain, Prefix } from '@/types/config.type.ts'
 import { useSearchStore } from '@/stores/search.ts'
 import GlowingBackButton from '@/components/GlowingBackButton.vue'
 import GlowingButtonBox from '@/components/GlowingButtonBox.vue'
+import type { Host, Prefix, Domain } from '@/types/config.type.ts'
+import { getConfig } from '@/api/config.api.ts'
+import { getPreferences } from '@/api/preferences.api.ts'
+import { createEmail } from '@/api/createEmail.api.ts'
 
 const router = useRouter()
 const searchStore = useSearchStore()
-
-const domains = ref<Domain[]>([])
-const preferences = ref<Preferences>()
-const config = ref<Host[]>([])
-const selectedHost = ref<Host | null>(null)
-
-const selectedDomain = ref<Domain | null>(null)
-const selectedPrefix = ref<Prefix | null>(null)
 
 function backToSearch() {
   searchStore.goSearch()
   router.push('/')
 }
 
-const password = ref('')
-const comment = ref('')
+const selectedHost = ref<Host | null>(null)
+
+const selectedPrefixId = ref<number | null>(null)
+const selectedDomainId = ref<number | null>(null)
+
+const prefix = computed<Prefix | null>(() => {
+  return selectedHost.value?.prefix.find((p) => p.id === selectedPrefixId.value) ?? null
+})
+
+const domain = computed<Domain | null>(() => {
+  return selectedHost.value?.domain.find((d) => d.id === selectedDomainId.value) ?? null
+})
 
 onMounted(async () => {
   try {
-    config.value = await getConfig()
-    domains.value = await getDomains()
-    preferences.value = await getPreferences()
-    selectedHost.value = config.value[0] ?? null
-    selectedDomain.value =
-      selectedHost.value?.domain.find((d) => d.id === preferences.value?.default_domain_id) ?? null
-    selectedPrefix.value =
-      selectedHost.value?.prefix.find((p) => p.id === preferences.value?.default_prefix_id) ?? null
-  } catch (error) {}
+    const config = await getConfig()
+    selectedHost.value = config[0] ?? null
+
+    const preferences = await getPreferences()
+
+    selectedPrefixId.value =
+      selectedHost.value?.prefix.find((p) => p.id === preferences.default_prefix_id)?.id ??
+      selectedHost.value?.prefix[0]?.id ??
+      null
+
+    selectedDomainId.value =
+      selectedHost.value?.domain.find((d) => d.id === preferences.default_domain_id)?.id ??
+      selectedHost.value?.domain[0]?.id ??
+      null
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 async function saveEmail() {
-  if (!selectedPrefix.value || !selectedDomain.value) return
+  console.log('Domain', selectedDomainId)
+  console.log('Prefix:', selectedPrefixId)
+  if (!selectedPrefixId.value || !selectedDomainId.value || !searchStore.needle) {
+    return
+  }
   try {
     const result = await createEmail({
-      prefix_id: selectedPrefix.value.id,
+      prefix_id: selectedPrefixId.value,
+      domain_id: selectedDomainId.value,
       name: searchStore.needle,
-      domain_id: selectedDomain.value.id,
-      password: password.value,
-      comment: comment.value,
     })
-
+    console.log(result)
     console.log(result.id)
+    await router.push(`/email-details/${result.id}`)
+
   } catch (error) {
     console.log(error)
   }
@@ -64,29 +76,30 @@ async function saveEmail() {
 <template>
   <div class="container">
     <h3>
-      {{ selectedPrefix?.name }}<span v-if="selectedPrefix?.name">.</span>{{ searchStore.needle
-      }}{{ selectedDomain?.name }}
+      {{ prefix?.name }}{{ prefix?.name ? '.' : '' }}{{ searchStore.needle }}{{ domain?.name }}
     </h3>
     <div class="column gap" style="margin-top: 1rem">
       <div class="row">
         <GlowingBackButton @click="backToSearch" icon="arrow_left_alt" class="btn-small" />
-        <GlowingButtonBox @click="saveEmail()" icon="mail" class="btn-small" />
+        <GlowingButtonBox icon="mail" class="btn-small" @click="saveEmail()" />
       </div>
       <input type="text" placeholder="neue E-Mail" v-model="searchStore.needle" />
     </div>
+
     <div class="column">
-      <div class="row" v-if="selectedHost && selectedHost?.domain.length > 1">
+      <div class="row" v-if="(selectedHost?.domain?.length ?? 0) > 1">
         <h3>Domain:</h3>
-        <select v-model="selectedDomain">
-          <option v-for="d in selectedHost?.domain" :key="d.id" :value="d">
+        <select v-model="selectedDomainId">
+          <option v-for="d in selectedHost?.domain" :key="d.id" :value="d.id">
             {{ d.name }}
           </option>
         </select>
       </div>
-      <div class="row" v-if="selectedHost && selectedHost?.prefix.length > 1">
+
+      <div class="row" v-if="(selectedHost?.prefix?.length ?? 0) > 1">
         <h3>Prefix:</h3>
-        <select v-model="selectedPrefix">
-          <option v-for="p in selectedHost?.prefix" :key="p.id" :value="p">
+        <select v-model="selectedPrefixId">
+          <option v-for="p in selectedHost?.prefix" :key="p.id" :value="p.id">
             {{ p.name }}
           </option>
         </select>
